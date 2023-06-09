4351e3df71f9268d15adff5b1ce8c5e10961c21e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-06-09 11:36:26 +0100

    progressss

--- counted-tech/src/app/app-routing.module.ts
+++ counted-tech/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardAllComponent } from './card-all/card-all.component';
+import { CardVacationComponent } from './card-vacation/card-vacation.component';
+import { CardAbsentComponent } from './card-absent/card-absent.component';
+import { CardInactiveComponent } from './card-inactive/card-inactive.component';
+import { CardAvailableComponent } from './card-available/card-available.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'card-all', component: CardAllComponent},
+  {path: 'card-available', component: CardAvailableComponent},
+  {path: 'card-inactive', component: CardInactiveComponent},
+  {path: 'card-absent', component: CardAbsentComponent},
+  {path: 'card-vacation', component: CardVacationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
