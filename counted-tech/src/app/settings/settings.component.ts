a995bff862bff7d236ec87720aa6b1073ec01e76
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-06-28 09:21:40 +0100

    rapping things up

--- counted-tech/src/app/settings/settings.component.ts
+++ counted-tech/src/app/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TermsConditionComponent } from '../terms-condition/terms-condition.component';
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
-
+  constructor(private router: Router) {}
+  openMainComponent() {
+    this.router.navigateByUrl('/');
+  }
+  openImprintComponent(){
+    this.router.navigateByUrl('/imprint')
+  }
+  openTermsConditionComponent(){
+    this.router.navigateByUrl('/terms-condition')
+  }
+  openPrivacyPolicyComponent(){
+    this.router.navigateByUrl('/privacy-policy')
+  }
+  openChangePasswordComponent(){
+    this.router.navigateByUrl('/change-password')
+  }
+  openLogoffComponent(){
+    this.router.navigateByUrl('/logoff')
+  }
+  openDeleteAccountComponent(){
+    this.router.navigateByUrl('/delete-account')
+  }
 }
